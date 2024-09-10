6a204ec1553435b2f302e5cece97bcf736a060c6
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-09-10 22:50:48 +0300

    SQL queries lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES 
+(1, 'Mr. Chocolate', 50, 'M', 'Blue', 'Killer', 0), 
+(2, 'Rowdy', 69, 'F', 'Blue', 'Killer', 1), 
+(3, 'TABITHA', 67, 'F', 'Blue', 'Killer', 1), 
+(4, 'Sergeant Brown', 58, 'M', 'Blue', 'Killer', 0), 
+(5, 'Melissa', 54, 'F', 'Blue', 'Killer', 1), 
+(6, 'Grinch', 55, 'M', 'Blue', 'Killer', 1), 
+(7, 'F', 'Wendy', 60, 'M', 'Blue', 'Killer', 0), 
+(8, 'Mr. Chocolate', 540, 'M', 'Blue', 'Killer', 0), 
+(9, NULL, 30, 'F', 'Black', 'Hostile', 1)
